013039d1a21ce339281e51a51b82aed186e12e08
James <james.t.farrell91@gmail.com>
2021-03-22 15:16:40 +0000

    Created Views and added in corresponding View Controllers

---
+++ Recipea/Controller/MyRecipes/MyRecipesRecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyRecipesRecipeViewController.swift
+//  Recipea
+//
+//  Created by James  Farrell on 22/03/2021.
+//  Copyright © 2021 James  Farrell. All rights reserved.
+//
+
+import UIKit
+
+class MyRecipesRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Recipea/Controller/RecipeaViewController.swift
+++ Recipea/Controller/RecipeaViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecipeaViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
